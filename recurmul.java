ec9192d6d4ae177a9bf7baeb716bbb1c4128e956
Maadeshwar <79583533+Maddyboii@users.noreply.github.com>
2026-08-25 15:51:03 +0530

    Add files via upload

---
+++ recurmul.java
@@ -0,0 +1,13 @@
+public class recurmul {
+    public static void mul(int n) {
+        if (n > 10) {
+            return;
+        }
+        int a = 7;
+        System.out.println(a+" x "+n+" = "+(a * n));
+        mul(n + 1);
+    }
+    public static void main(String[] args) {
+        mul(1);
+    }
+}
